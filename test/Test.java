62be1e4928ed13d5e0210e62f3a9aff66a2dd7cd
woo159753 <gyungsik1346@gmail.com>
2026-09-03 09:21:24 +0900

    우경식 브랜치 테스트입니다

---
+++ test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Test {
+    
+    public static void main(String[] args) {
+        System.out.println("테스트 파일입니다.");
+        System.out.println("마스터 브랜치 파일 테스트입니다.");
+        System.err.println("우경식 브랜치 테스트입니다.");
+    }
+}
