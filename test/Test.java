b70e2b91c32a4f0119c70c9d51ac26ef76f82f58
kms73289-cmyk <kms73289@gmail.com>
2026-09-03 09:41:06 +0900

    최종 테스트입니다

--- test/Test.java
+++ test/Test.java
@@ -5,5 +5,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("테스트 파일입니다.");
         System.out.println("김민서 브랜치 파일 테스트입니다.");
+        System.out.println("김민서 브랜치 파일 4차 테스트입니다.");
+        System.out.println("최종 테스트입니다");
     }
 }
