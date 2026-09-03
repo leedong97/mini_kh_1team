fc6318f9e57d7854e3d4190348ca2ea80dfe0158
thedayfly1-crypto <thedayfly1@naver.com>
2026-09-03 11:01:48 +0900

    커밋되고잇지?

--- test/Test.java
+++ test/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("테스트 파일입니다.");
         System.out.println("김민서 브랜치 파일 테스트입니다.");
+        System.out.println("나원씨 커밋 확인중입니다.");
     }
 }
