ad8e8679715a7328237003b18200e88c498e4b7a
thedayfly1-crypto <thedayfly1@naver.com>
2026-09-03 11:09:00 +0900

    2차 테스트

--- test/Test.java
+++ test/Test.java
@@ -6,5 +6,6 @@ public class Test {
         System.out.println("테스트 파일입니다.");
         System.out.println("김민서 브랜치 파일 테스트입니다.");
         System.out.println("나원씨 커밋 확인중입니다.");
+        System.out.println("나원씨 커밋 확인 2차");
     }
 }
